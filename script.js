ce2d89f967ce665f3c02b920136b0b01f0b93b75
dumbprogrammer <102885633+Lilafian@users.noreply.github.com>
2023-03-01 21:41:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ const letters = "abcdefghijklmnopqrstuvwxyz";
 
 let interval = null;
 
-document.querySelector("h1").onmouseover = event => {  
+function randomText() {  
+  
+  let elementtitle = document.getElementById("maintitle");
   let iteration = 0;
   
   clearInterval(interval);
   
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    elementtitle.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return elementtitle.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
+    if(iteration >= elementtitle.dataset.value.length){ 
       clearInterval(interval);
     }
     
